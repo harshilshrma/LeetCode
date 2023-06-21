0460d7bc9cff3eb43f779509ca00d4ef7a4ecef3
Harshil <harshilsharma.2020@gmail.com>
2023-06-21 16:44:35 +0530

    Constructed the HashMap Logic

--- src/Medium/Q26_TopKFrequentElements.java
+++ src/Medium/Q26_TopKFrequentElements.java
@@ -7,24 +7,47 @@ import java.util.*;
 
 public class Q26_TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        int count = 0;
+        int count = 0, max = 1;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i=0; i<nums.length; i++) {
-            if(!map.containsKey(nums[i])) {
-                map.put(nums[i], count+1);
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, count);
             }
-            map.;
+            map.put(num, map.get(num) + 1);
         }
         System.out.println(map);
-        int[] numArray = new int[]{1, 1, 1, 2, 2, 3};
-        return numArray;
+
+        Integer[] keyArray = new Integer[map.size()];
+        Integer[] valueArray = new Integer[map.size()];
+
+        int i = 0;
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            keyArray[i] = key;
+            valueArray[i] = value;
+            i++;
+        }
+        for (i=0; i<map.size(); i++) {
+            for (int j=0; j<map.size(); j++) {
+                int
+
+
+            }
+        }
+
+
+
+
+
+        return new int[]{1, 1, 1, 2, 2, 3};
     }
 
     public static void main(String[] args) {
         Q26_TopKFrequentElements obj1 = new Q26_TopKFrequentElements();
         int k = 3;
-        int[] numArray = new int[]{1, 1, 1, 2, 2, 3};
-        obj1.topKFrequent(numArray, k);
+        int[] num = new int[]{1, 1, 2, 2, 2, 2, 3, 4, 4};
+        obj1.topKFrequent(num, k);
 
     }
 
