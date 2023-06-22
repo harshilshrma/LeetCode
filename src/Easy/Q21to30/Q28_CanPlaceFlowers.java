6a4aa3a1701401ff35535020f9c8728c669e144b
Harshil <harshilsharma.2020@gmail.com>
2023-06-22 17:03:01 +0530

    Q28: First Commit

---
+++ src/Easy/Q21to30/Q28_CanPlaceFlowers.java
@@ -0,0 +1,12 @@
+// Date: June 22, 2023
+// Q28: You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers
+// cannot be planted in adjacent plots.
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an
+// integer n, return true if n new flowers can be planted in the flowerbed without violating the
+// no-adjacent-flowers rule and false otherwise.
+
+package Easy.Q21to30;
+
+public class Q28_CanPlaceFlowers {
+
+}
