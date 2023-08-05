bbc899a2d47f47a4d8f4c40a92335493f1075c05
Harshil <harshilsharma.2020@gmail.com>
2023-08-06 00:07:08 +0530

    Q33: Final working code

--- src/Medium/Q33_SetMatrixZeroes.java
+++ src/Medium/Q33_SetMatrixZeroes.java
@@ -12,8 +12,8 @@ public class Q33_SetMatrixZeroes {
     static boolean setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int row[] = new int[m];
-        int col[] = new int[n];
+        int[] row = new int[m];
+        int[] col = new int[n];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(matrix[i][j] == 0){
