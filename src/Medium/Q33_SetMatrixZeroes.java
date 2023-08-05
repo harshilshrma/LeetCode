13497f9540e42739de30e68e7dc22d0bf1c6bfc6
Harshil <harshilsharma.2020@gmail.com>
2023-08-06 00:06:46 +0530

    Q33: Final working code

--- src/Medium/Q33_SetMatrixZeroes.java
+++ src/Medium/Q33_SetMatrixZeroes.java
@@ -6,76 +6,38 @@
 
 package Medium;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class Q33_SetMatrixZeroes {
-    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
-        // int[] row = new int[n]; --> matrix[..][0]
-        // int[] col = new int[m]; --> matrix[0][..]
-
-        int col0 = 1;
-        // step 1: Traverse the matrix and
-        // mark 1st row & col accordingly:
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix.get(i).get(j) == 0) {
-                    // mark i-th row:
-                    matrix.get(i).set(0, 0);
-
-                    // mark j-th column:
-                    if (j != 0)
-                        matrix.get(0).set(j, 0);
-                    else
-                        col0 = 0;
+    static boolean setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row[] = new int[m];
+        int col[] = new int[n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-
-        // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix.get(i).get(j) != 0) {
-                    // check for col & row:
-                    if (matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0) {
-                        matrix.get(i).set(j, 0);
-                    }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
                 }
             }
         }
-
-        //step 3: Finally mark the 1st col & then 1st row:
-        if (matrix.get(0).get(0) == 0) {
-            for (int j = 0; j < m; j++) {
-                matrix.get(0).set(j, 0);
-            }
-        }
-        if (col0 == 0) {
-            for (int i = 0; i < n; i++) {
-                matrix.get(i).set(0, 0);
-            }
-        }
-
-        return matrix;
+        System.out.println(Arrays.deepToString(matrix));
+        return false;
     }
 
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
-        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1, 0)));
-        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1, 2)));
-        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1, 3)));
-        matrix.add(new ArrayList<>(Arrays.asList(2, 1, 0, 2)));
-
-        int n = matrix.size();  //no. of rows
-        int m = matrix.get(0).size(); //no. of columns
-
-        ArrayList<ArrayList<Integer>> ans = zeroMatrix(matrix, n, m);
-
-        System.out.println("The Final matrix is: ");
-        for (ArrayList<Integer> row : ans) {
-            for (Integer ele : row) {
-                System.out.print(ele + " ");
-            }
-            System.out.println();
-        }
+        int[][] arr = {
+                {1, 3, 1},
+                {0, 1, 3}
+        };
+        System.out.println(setZeroes(arr));
     }
 }
