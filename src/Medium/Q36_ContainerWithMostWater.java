49fba71760441d116930245142e8919332d75de1
Harshil <harshilsharma.2020@gmail.com>
2023-12-09 16:36:06 +0530

    Q36: Initial problem structure

---
+++ src/Medium/Q36_ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+// Date: 09 December 2023
+// You are given an integer array height of length n. There are n vertical lines drawn such that the
+// two endpoints of the ith line are (i, 0) and (i, height[i]).Find two lines that together with the x-axis
+// form a container, such that the container contains the most water.
+// Return the maximum amount of water a container can store.
+// Notice that you may not slant the container.
+
+package Medium;
+
+public class Q36_ContainerWithMostWater {
+    public int maxArea(int[] height) {
+
+
+
+        return 0;
+    }
+
+    // Main function for testing
+    public static void main(String[] args) {
+
+    }
+
+}
