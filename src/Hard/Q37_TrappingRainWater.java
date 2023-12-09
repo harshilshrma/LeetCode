64208d27361ee4e9d08b6da1037e1c502807b37c
Harshil <harshilsharma.2020@gmail.com>
2023-12-09 18:49:01 +0530

    Q37: Initial code

---
+++ src/Hard/Q37_TrappingRainWater.java
@@ -0,0 +1,18 @@
+// Date: 09 December 2023
+// Given n non-negative integers representing an elevation map where the width of each bar is 1,
+// compute how much water it can trap after raining.
+
+package Hard;
+
+public class Q37_TrappingRainWater {
+    public int trap(int[] height) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
