68a92b0853816ab632ea5719184209368396188e
Harshil <harshilsharma.2020@gmail.com>
2023-12-12 21:51:57 +0530

    Q40: Final working code

--- src/Medium/Q40_KokoEatingBananas.java
+++ src/Medium/Q40_KokoEatingBananas.java
@@ -7,18 +7,40 @@
 // Return the minimum integer k such that she can eat all the bananas within h hours.
 
 package Medium;
+import java.util.Arrays;
 
 public class Q40_KokoEatingBananas {
     public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int l = 1;
+        int r = piles[piles.length - 1];
+        int res = r; // not initialising it to 0 but to r(max) because this is the max which our
+        // answer can be and we have to find the minimum
 
+        // Applying the binary search
+        while (l <= r) {
+            int k = (l + r) / 2;
+            int hours = 0;
+            for (int i : piles) {
+                // We have to round up, so we will use Math.ceil
+                // Ex: Ceil(7.0 / 3.0) = Ceil(2.333) = 3.0 -> 3 in int
+                hours += Math.ceil((double) i / k);
+            }
 
-        return 0;
+            if (hours <= h) { // Meaning Koko ate faster, she could have gone slower
+                res = Math.min(res, k);
+                r = k - 1;
+            } else {
+                l = k + 1;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         Q40_KokoEatingBananas obj = new Q40_KokoEatingBananas();
-        int[] piles = {3,6,7,11};
-        int h = 8;
+        int[] piles = {30, 11, 23, 4, 20};
+        int h = 6;
         System.out.println(obj.minEatingSpeed(piles, h));
     }
 }
