b72f6eff917a1a4cb7609f2293ebc50f3433846b
Harshil <harshilsharma.2020@gmail.com>
2023-12-17 15:30:35 +0530

    0003 - initial code

---
+++ src/Medium/lc0003_LongestSubstrWithoutRepeatingChars.java
@@ -0,0 +1,18 @@
+// Date: 17 December 2023
+// Given a string s, find the length of the longest substring without repeating characters.
+
+package Medium;
+
+public class lc0003_LongestSubstrWithoutRepeatingChars {
+    public int lengthOfLongestSubstring(String s) {
+
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        lc0003_LongestSubstrWithoutRepeatingChars obj = new lc0003_LongestSubstrWithoutRepeatingChars();
+        String str1 = "abcabcbb";
+        System.out.println(obj.lengthOfLongestSubstring(str1));
+    }
+}
