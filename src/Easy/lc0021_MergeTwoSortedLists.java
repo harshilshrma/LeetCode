5f09486fe6a0faacf4699a05e146bfb69b6f1d63
Harshil <harshilsharma.2020@gmail.com>
2023-12-21 13:24:02 +0530

    0021 - initial code

---
+++ src/Easy/lc0021_MergeTwoSortedLists.java
@@ -0,0 +1,21 @@
+// Date: 21 December 2023
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the
+// first two lists. Return the head of the merged linked list.
+
+package Easy;
+
+public class lc0021_MergeTwoSortedLists {
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+}
