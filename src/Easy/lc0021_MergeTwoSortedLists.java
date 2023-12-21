7043db1b86c57162eb7335814aa3eb56920fc7f6
Harshil <harshilsharma.2020@gmail.com>
2023-12-21 15:53:51 +0530

    0021 - final working code

--- src/Easy/lc0021_MergeTwoSortedLists.java
+++ src/Easy/lc0021_MergeTwoSortedLists.java
@@ -10,12 +10,53 @@ public class lc0021_MergeTwoSortedLists {
     public class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    public ListNode mergeTwoListsByIteration(ListNode list1, ListNode list2) {
+        final ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                // Adding to tail
+                tail.next = list1;
+                // Updating list1 pointer
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            // tail pointer is updated no matter what
+            tail = tail.next;
+        }
+        // Concatenation of Remaining Nodes
+        tail.next = list1 != null
+                ? list1
+                : list2;
+        return dummy.next;
+    }
 
+    public ListNode mergeTwoListsByRecursion(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoListsByRecursion(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoListsByRecursion(list2.next, list1);
+            return list2;
+        }
     }
 }
