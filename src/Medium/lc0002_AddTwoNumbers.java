467289f420f2b61ffce034f5dbd6eb7d4ec594f2
Harshil Sharma <harshilsharma.dev@gmail.com>
2023-12-25 16:04:10 +0530

    0002 - initial code

---
+++ src/Medium/lc0002_AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package Medium;
+
+public class lc0002_AddTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+}
