f9911cb2d110474f71f51ef6aa57dbf3bd51b8b6
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-01 16:29:44 +0530

    0155 - final working code

---
+++ src/Medium/lc0155_MinStack.java
@@ -0,0 +1,44 @@
+// Date: 1 January 2024 (HAPPY NEW YEAR!)
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+// Implement the MinStack class:
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+package Medium;
+import java.util.Stack;
+
+public class lc0155_MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack ;
+
+    public lc0155_MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        // minStack may be empty so we need to check it
+        val = Math.min(val, minStack.isEmpty() ? val : minStack.peek());
+        minStack.push(val);
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
