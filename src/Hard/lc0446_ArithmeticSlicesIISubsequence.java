3be89f4270bc34b629473902596886cb33fbabb0
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-07 17:47:19 +0530

    0446 - initial code

---
+++ src/Hard/lc0446_ArithmeticSlicesIISubsequence.java
@@ -0,0 +1,27 @@
+// Date: 7 January 2024
+// Given an integer array nums, return the number of all the arithmetic subsequences of nums.
+//A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference
+// between any two consecutive elements is the same.
+//For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are arithmetic sequences.
+//For example, [1, 1, 2, 5, 7] is not an arithmetic sequence.
+//A subsequence of an array is a sequence that can be formed by removing some elements (possibly none) of the array.
+//For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
+//The test cases are generated so that the answer fits in 32-bit integer.
+
+package Hard;
+
+public class lc0446_ArithmeticSlicesIISubsequence {
+    public int numberOfArithmeticSlices(int[] nums) {
+
+
+
+
+    }
+
+    // Main fucntion for testing
+    public static void main(String[] args) {
+        lc0446_ArithmeticSlicesIISubsequence obj = new lc0446_ArithmeticSlicesIISubsequence();
+        int[] nums = {2,4,6,8,10};
+        System.out.println(obj.numberOfArithmeticSlices(nums));
+    }
+}
