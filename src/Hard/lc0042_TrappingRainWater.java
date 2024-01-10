67bf31aa125b8ed686afad52bd08d740b342f8c1
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-11 01:17:36 +0530

    0042 - revision 1: added both (bruteForce+optimal) solutions

--- src/Hard/lc0042_TrappingRainWater.java
+++ src/Hard/lc0042_TrappingRainWater.java
@@ -4,11 +4,33 @@
 
 package Hard;
 
+import java.util.Arrays;
+
 public class lc0042_TrappingRainWater {
     public int trap(int[] height) {
-        // For Null input array
-        if (height.length == 0) return 0;
 
+        // Brute Force (using Arrays)
+//        int[] maxLeft = new int[height.length];
+//        int[] maxRight = new int[height.length];
+//        maxLeft[0] = height[0];
+//        maxRight[maxRight.length - 1] = height[height.length - 1];
+//
+//        for (int i = 1; i < height.length; i++) {
+//            maxLeft[i] = Math.max(height[i], maxLeft[i - 1]);
+//        }
+//
+//        for (int i = maxRight.length - 2; i >= 0; i--) {
+//            maxRight[i] = Math.max(height[i], maxRight[i + 1]);
+//        }
+//
+//        int water = 0;
+//        for (int i = 0; i < height.length; i++) {
+//            water += (Math.min(maxLeft[i], maxRight[i]) - height[i]);
+//        }
+//
+//        return water;
+
+        // Optimal (Using Two Pointers)
         int l = 0;
         int r = height.length - 1;
         int maxLeft = height[l];
@@ -16,12 +38,11 @@ public class lc0042_TrappingRainWater {
         int water = 0;
 
         while (l < r) {
-            if (maxLeft < maxRight){
+            if (maxLeft < maxRight) {
                 l++;
                 maxLeft = Math.max(maxLeft, height[l]);
                 water += maxLeft - height[l];
-            }
-            else {
+            } else {
                 r--;
                 maxRight = Math.max(maxRight, height[r]);
                 water += maxRight - height[r];
@@ -34,6 +55,6 @@ public class lc0042_TrappingRainWater {
         lc0042_TrappingRainWater obj1 = new lc0042_TrappingRainWater();
         int[] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
         int[] arr2 = {4,2,0,3,2,5};
-        System.out.println(obj1.trap(arr2));
+        System.out.println(obj1.trap(arr));
     }
 }
