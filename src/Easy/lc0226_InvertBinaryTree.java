713db7616bf3558ca4cb58fab96ec241da9e4ad3
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-12 18:54:41 +0530

    0226 - final working code

--- src/Easy/lc0226_InvertBinaryTree.java
+++ src/Easy/lc0226_InvertBinaryTree.java
@@ -41,18 +41,21 @@ public class lc0226_InvertBinaryTree {
     }
 
     public TreeNode invertTree(TreeNode root) {
+        // Base case: If the root is null, return null
         if (root == null) {
             return null;
         }
 
-        // swapping the children
+        // Swap the left and right children of the current node
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
 
+        // Recursively invert the left and right subtrees
         invertTree(root.left);
         invertTree(root.right);
 
+        // Return the inverted tree
         return root;
     }
 
