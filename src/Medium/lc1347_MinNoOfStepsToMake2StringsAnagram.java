e392209259864197c97a7dd26e35768a387094b3
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-13 15:20:23 +0530

    1347 - initial code

---
+++ src/Medium/lc1347_MinNoOfStepsToMake2StringsAnagram.java
@@ -0,0 +1,19 @@
+// Date: 13 January 2024
+// You are given two strings of the same length s and t. In one step you can choose any character of t and replace
+// it with another character.
+//Return the minimum number of steps to make t an anagram of s.
+//An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+package Medium;
+
+public class lc1347_MinNoOfStepsToMake2StringsAnagram {
+    public int minSteps(String s, String t) {
+
+    }
+
+    // Main function for testing
+    public static void main(String[] args) {
+        lc1347_MinNoOfStepsToMake2StringsAnagram obj = new lc1347_MinNoOfStepsToMake2StringsAnagram();
+
+    }
+}
