0e5a7be70e20a1979bdedb0f81155a197840710c
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-13 14:31:02 +0530

    0104 - added comments

--- src/Easy/lc0104_MaximumDepthOfBinaryTree.java
+++ src/Easy/lc0104_MaximumDepthOfBinaryTree.java
@@ -17,10 +17,15 @@ public class lc0104_MaximumDepthOfBinaryTree {
         }
     }
 
-    public int maxDepthByRecursion(TreeNode root) {
+    // Function to calculate the maximum depth of a binary tree
+    public int maxDepth(TreeNode root) {
+        // Base case: If the root is null, the depth is 0
         if (root == null) {
             return 0;
         }
-        return 1 + Math.max((maxDepthByRecursion(root.left)), maxDepthByRecursion(root.right));
+
+        // Recursive calculation of the maximum depth
+        // Return 1 (current level) plus the maximum depth of the left or right subtree
+        return 1 + Math.max((maxDepth(root.left)), maxDepth(root.right));
     }
 }
