b4647476944396372e5370112357314a62ceebbb
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-13 16:29:13 +0530

    1347 - final working code

--- src/Medium/lc1347_MinNoOfStepsToMake2StringsAnagram.java
+++ src/Medium/lc1347_MinNoOfStepsToMake2StringsAnagram.java
@@ -5,15 +5,44 @@
 //An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
 
 package Medium;
+import java.util.HashMap;
 
 public class lc1347_MinNoOfStepsToMake2StringsAnagram {
     public int minSteps(String s, String t) {
+        int ans = 0;
 
+        // Count the frequency of characters in s
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            mapS.put(c, mapS.getOrDefault(c, 0) + 1);
+        }
+
+        // Count the frequency of characters in t
+        HashMap<Character, Integer> mapT = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            mapT.put(c, mapT.getOrDefault(c, 0) + 1);
+        }
+
+        // Iterate through the characters in mapS
+        for (char c : mapS.keySet()) {
+            int freqS = mapS.get(c);
+            int freqT = mapT.getOrDefault(c, 0);
+
+            // If the frequency in t is less than the frequency in s, update the answer
+            if (freqT < freqS) {
+                ans += freqS - freqT;
+            }
+        }
+
+        // Return the final result representing the difference in character frequencies
+        return ans;
     }
 
     // Main function for testing
     public static void main(String[] args) {
         lc1347_MinNoOfStepsToMake2StringsAnagram obj = new lc1347_MinNoOfStepsToMake2StringsAnagram();
-
+        String s = "aba";
+        String t = "bba";
+        System.out.println(obj.minSteps(s, t));
     }
 }
