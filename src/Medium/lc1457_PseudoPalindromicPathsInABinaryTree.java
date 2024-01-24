4cf9382b3c59bb03de76a2823885b2578f961948
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-24 12:58:24 +0530

    1457 - initial commit

---
+++ src/Medium/lc1457_PseudoPalindromicPathsInABinaryTree.java
@@ -0,0 +1,26 @@
+// Date: 24 January 2024
+// Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be
+// pseudo-palindromic if at least one permutation of the node values in the path is a palindrome.
+//Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
+
+package Medium;
+
+public class lc1457_PseudoPalindromicPathsInABinaryTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode right;
+        TreeNode left;
+        TreeNode(int val, TreeNode right, TreeNode left) {
+            this.val = val;
+            this.right = right;
+            this.left = left;
+        }
+    }
+
+    public int pseudoPalindromicPaths (TreeNode root) {
+
+
+
+    }
+}
