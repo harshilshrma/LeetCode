b69211beac0e2e4cb53de5c81ea9516ce8583648
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-25 17:37:55 +0530

    235 - initial code

---
+++ src/Medium/lc0235_LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,20 @@
+package Medium;
+
+public class lc0235_LowestCommonAncestorOfABinarySearchTree {
+    public static class TreeNode {
+        int val;
+        TreeNode right;
+        TreeNode left;
+        TreeNode (int val, TreeNode right, TreeNode left) {
+            this.val = val;
+            this.right = right;
+            this.left = left;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+
+        return root;
+    }
+}
