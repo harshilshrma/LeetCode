6f13c714dc45d83d97a5941d90c6ea7fab799f2c
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-03-25 17:40:28 +0530

    53 - added the code to print the max subarray

--- src/Medium/lc0053_MaximumSubarray.java
+++ src/Medium/lc0053_MaximumSubarray.java
@@ -10,16 +10,26 @@ public class lc0053_MaximumSubarray {
     public int maxSubArray(int[] nums) {
         // Initialize variables to store the maximum sum and the current sum
         int max = Integer.MIN_VALUE;
-        int sum = 0;
+        int sum = 0, start = 0, ansStart = -1, ansEnd = -1;
 
         // Iterate through the array
         for (int i = 0; i < nums.length; i++) {
+
+            // to print
+            if (sum == 0) {
+                start = i;
+            }
+
             // Add the current element to the current sum
             sum += nums[i];
 
             // Update the maximum sum if the current sum is greater
             if (sum > max) {
                 max = sum;
+
+                // Marking for printing
+                ansStart = start;
+                ansEnd = i;
             }
 
             // If the current sum becomes negative, reset it to zero
@@ -28,6 +38,13 @@ public class lc0053_MaximumSubarray {
             }
         }
 
+        // Printing the max sum subarray
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
+
         // Return the maximum sum found
         return max;
     }
@@ -35,7 +52,7 @@ public class lc0053_MaximumSubarray {
     // Main function for testing
     public static void main(String[] args) {
         lc0053_MaximumSubarray obj = new lc0053_MaximumSubarray();
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] nums = {5, 4, -1, 7, 8};
         System.out.println(obj.maxSubArray(nums));
     }
 }
