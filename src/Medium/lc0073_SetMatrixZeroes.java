fd59f48ef6c80a47f79b02bbc635993c373f4d45
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-04-01 23:26:26 +0530

    73 - added optimal approach

--- src/Medium/lc0073_SetMatrixZeroes.java
+++ src/Medium/lc0073_SetMatrixZeroes.java
@@ -90,7 +90,7 @@ public class lc0073_SetMatrixZeroes {
     public static void main(String[] args) {
         int[][] arr = {
                 {1, 3, 1},
-                {0, 1, 3}
+                {0, 1, 2}
         };
         System.out.println(setZeroes(arr));
         System.out.println(setZeroesOptimaal(arr));
