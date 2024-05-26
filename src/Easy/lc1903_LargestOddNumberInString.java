1193c5d756eaf8aae3a0d89d4f66b108a54725fc
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-05-26 23:39:36 +0530

    1903 - final working code

--- src/Easy/lc1903_LargestOddNumberInString.java
+++ src/Easy/lc1903_LargestOddNumberInString.java
@@ -1,5 +1,7 @@
 // 26-05-2024 - 11:03 pm
-// 
+// You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+//
+//A substring is a contiguous sequence of characters within a string.
 
 package Easy;
 
