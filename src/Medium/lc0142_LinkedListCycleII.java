cee0d6791aea9c73bbe385d8c88c965d0a1cc140
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-05-30 01:18:15 +0530

    142 - final working code

---
+++ src/Medium/lc0142_LinkedListCycleII.java
@@ -0,0 +1,37 @@
+// 29-05-2024 - 06:18 pm
+// 
+
+package Medium;
+
+public class lc0142_LinkedListCycleII {
+    // Functions
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+
+
+
